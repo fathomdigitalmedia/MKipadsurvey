29072dae9744861ab0ef6879914611567cdc3d21
Patrick Shea <patrick@fathomdigitalmedia.com>
2015-01-04 14:02:40 -0600

    Initial Commit

---
+++ H&R Block Survey/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  H&R Block Survey
+//
+//  Created by Patrick Shea on 1/4/15.
+//  Copyright (c) 2015 MotionKick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ H&R Block SurveyTests/H_R_Block_SurveyTests.m
@@ -0,0 +1,40 @@
+//
+//  H_R_Block_SurveyTests.m
+//  H&R Block SurveyTests
+//
+//  Created by Patrick Shea on 1/4/15.
+//  Copyright (c) 2015 MotionKick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface H_R_Block_SurveyTests : XCTestCase
+
+@end
+
+@implementation H_R_Block_SurveyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
