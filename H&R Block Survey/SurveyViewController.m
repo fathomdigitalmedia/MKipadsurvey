8f4610a9a93498f953d101862243811f71c87422
Patrick Shea <patrick@fathomdigitalmedia.com>
2015-01-05 16:48:18 -0600

    survey view changes, cont'd

--- H&R Block Survey/SurveyViewController.m
+++ H&R Block Survey/SurveyViewController.m
@@ -10,6 +10,7 @@
 // #import "Survey2ViewController.h"
 
 @interface SurveyViewController ()
+@property (weak, nonatomic) IBOutlet UIView *surveyBG;
 
 @end
 
@@ -22,12 +23,31 @@
     // init survey object (UID, question strings (en, sp), pretty date string, data file name, etc)
     // animate survey view elements on screen
     
+    [self animateOnLoad];
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    // add code to check that screen visual elements are visible and in the proper x,y positions (after segue unwind)
+
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the survey object to the new view controller.
     
+}
+
+- (void)animateOnLoad {
+    NSLog(@"animateOnLoad survey x = %f", self.surveyBG.frame.origin.x);
+    
+    [UIView animateWithDuration:5 animations:^{
+        NSLog(@"animateOnLoad animation block");
+        CGRect surveyBGFrame = self.surveyBG.frame;
+        surveyBGFrame.origin.x = 0;
+        self.surveyBG.frame = surveyBGFrame;
+        self.surveyBG.alpha = 0;
+    }];
 }
 
 
